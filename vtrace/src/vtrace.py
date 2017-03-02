4ec336419d39976839f1e68f862328885d6b032b
Christian Thaler <christian.thaler@tes-dst.com>
2017-03-02 17:06:56 +0100

    Support for mapping functions.
    Symbols from shared libraries have to be mapped to the same position they have been mapped to during trace recording.

--- vtrace/src/vtrace.py
+++ vtrace/src/vtrace.py
@@ -2,14 +2,18 @@ import re
 
 ftraces = []
 fnames = {}
+symbols = []
+mapping = []
+offset = 0
 
 def parse_line(l):
-	m = re.match(r"(\d+)(\<|\>)0x([0-9A-Fa-f]+)", l)
+	m = re.match(r"(\-?\d+)(\<|\>)0x([0-9A-Fa-f]+)", l)
 	ftraces.append(m.groups())
-	fnames[int(m.groups()[2], 16)] = "func_" + m.groups()[2]
+	fnames[long(m.groups()[2], 16)] = "func_" + m.groups()[2]
 
 
 def parse(filename):
+	global mapping
 	with open(filename) as f:
 		# Read header and date
 		header = f.readline()
@@ -17,23 +21,35 @@ def parse(filename):
 			print "ERROR"
 
 		date = f.readline()
+		mapping = f.readline().split()
+		data = f.readline()
 
 		# Read function traces
 		for l in f:
 			parse_line(l)
 
 
-def parse_symbol(s):
-	addr = int(s[0], 16)
-	name = s[3]
-	if addr in fnames:
-		fnames[addr] = name
+def parse_symbols():
+	global offset, symbols
+	for s in symbols:
+		addr = long(s[0], 16) + offset
+		name = s[3]
+		if addr in fnames:
+			fnames[addr] = name
+			print "added " + name + " " + format(addr,"x")
 	
 
-def load_symbols():
-	with open("../test/test.sym") as f:
+def compute_offset(fname, pos):
+	global symbols, offset
+	for n in symbols:
+		if n[3] == fname:
+			offset = pos - long(n[0], 16)
+
+def load_symbols(fsym):
+	global symbols
+	with open(fsym) as f:
 		for l in f:
-			parse_symbol(l.split())
+			symbols.append(l.split())
 
 
 def write_vcd():
@@ -70,6 +86,10 @@ def write_vcd():
 			print "0f" + t[2]
 		
 
-parse("../test/test.vtrc")
-load_symbols()
-write_vcd()
+parse("./test.vtrc")
+load_symbols("./test.sym")
+compute_offset(mapping[1], long(mapping[2],16))
+print offset
+parse_symbols()
+#print fnames
+#write_vcd()
