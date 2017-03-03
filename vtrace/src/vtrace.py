907d46f9776ff3d96598bc08aed440b975425e07
c-thaler <bummberumm@googlemail.com>
2017-03-03 02:48:54 +0100

    Added statistics mode.
    Accumulating each function execution time.

--- vtrace/src/vtrace.py
+++ vtrace/src/vtrace.py
@@ -1,3 +1,4 @@
+import sys
 import re
 import argparse
 
@@ -5,13 +6,15 @@ ftraces = []
 fnames = {}
 symbols = []
 mapping = []
+timings = []
 offset = 0
 
 def parse_line(l):
 	global fnames
 	m = re.match(r"(\-?\d+)(\<|\>)0x([0-9A-Fa-f]+)", l)
-	ftraces.append(m.groups())
-	fnames[long(m.groups()[2], 16)] = "func_" + m.groups()[2]
+	grp = m.groups()
+	ftraces.append((long(grp[0], 10), grp[1], long(grp[2], 16)))
+	fnames[long(grp[2], 16)] = "func_" + grp[2]
 
 
 def parse(filename):
@@ -26,7 +29,7 @@ def parse(filename):
 		mapping = f.readline().split()
 		data = f.readline()
 
-		print "Parsing vtrace data from " + filename + "..."
+		sys.stderr.write("Parsing vtrace data from " + filename + "\n")
 		# Read function traces
 		for l in f:
 			parse_line(l)
@@ -34,7 +37,7 @@ def parse(filename):
 
 def parse_symbols():
 	global offset, symbols
-	print "Parsing symbols..."
+	sys.stderr.write("Parsing symbols...\n")
 	for s in symbols:
 		addr = long(s[0], 16) + offset
 		name = s[3]
@@ -51,10 +54,29 @@ def compute_offset(fname, pos):
 def load_symbols(fsym):
 	global symbols
 	with open(fsym) as f:
-		print "Loading symbols from " + fsym + "..."
+		sys.stderr.write("Loading symbols from " + fsym + "\n")
 		for l in f:
 			symbols.append(l.split())
 
+
+def accumulate_timings(faddr):
+	global ftraces, fnames, timings
+	calc = False
+	total = long(0)
+	for t in ftraces:
+		if t[2] == faddr:
+			if calc:
+				total += t[0] - start
+				calc = False
+			else:
+				start = t[0]
+				calc = True
+	timings.append((fnames[faddr], total))
+
+def compute_timings():
+	global fnames
+	for f in fnames.keys():
+		accumulate_timings(f)
 
 def write_vcd():
 	print 	"$date\n"\
@@ -75,7 +97,7 @@ def write_vcd():
 	print	"$upscope $end\n"\
 		"$enddefinitions $end"
 
-	print	"#" + ftraces[0][0]
+	print	"#" + format(ftraces[0][0], "d")
 	print	"$dumpvars"
 	for f in fnames.keys():
 		print "0f" + format(f, "x") 
@@ -83,7 +105,7 @@ def write_vcd():
 
 
 	for t in ftraces:
-		print "#" + t[0]
+		print "#" + format(t[0], "d")
 		if t[1] == ">":
 			print "1f" + t[2]
 		else:
@@ -95,6 +117,8 @@ parser.add_argument("vtrace", type=str,
                     help="the vtrace file")
 parser.add_argument("symbols", type=str,
                     help="file containing the symbols of the traced binary")
+parser.add_argument("-s", "--statistics", action="store_true",
+                    help="print statistics")
 parser.add_argument("-v", "--verbose", action="store_true",
                     help="increase output verbosity")
 args = parser.parse_args()
@@ -118,6 +142,15 @@ if args.verbose:
 	print "Offset: " + format(offset,"x")
 
 parse_symbols()
-write_vcd()
+
+if args.statistics:
+	print ""
+	print "Statistics:"
+	compute_timings()
+	sorted_timings = sorted(timings, key=lambda time: time[1])
+	for t in sorted_timings:
+		print t[0] + " " + format(t[1], "d") + " ns"
+else:
+	write_vcd()
 
 
